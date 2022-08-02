ea8a969bba10216fe80f2025c8a6fff49946f46a
Matthew Grohman <56929777+mgrohman13@users.noreply.github.com>
2022-08-02 10:19:32 -0700

    missing file

---
+++ HOMM3/Log.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOMM3
+{
+    public class Log
+    {
+        private readonly static List<string> logs = new();
+        public static void Out(string format, params object[] args)
+        {
+            Out(2, format, args);
+        }
+        public static void Out(int digits, string format, params object[] args)
+        {
+            string FormatDigits(object a) => a is double d ? d.ToString("0." + new string('0', digits)) : a.ToString();
+            args = args.Select(a =>
+            {
+                IEnumerable<object> enumerable;
+                if (a is IList l && (enumerable = Enumerable.OfType<object>(l)).Any())
+                    return "[" + enumerable.Select(FormatDigits).Aggregate((b, c) => b.ToString() + "," + c.ToString()) + "]";
+                return a;
+            }).Select(FormatDigits).ToArray();
+            logs.Add(string.Format(format, args));
+        }
+        public static void Write(string file)
+        {
+            File.WriteAllText(file, logs.Aggregate((a, b) => a + Environment.NewLine + b));
+        }
+    }
+}
