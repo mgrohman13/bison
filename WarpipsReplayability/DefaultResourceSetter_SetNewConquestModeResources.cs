a600e5271d03f9223a55ae5c5eaf27987b0f77be
Matthew Grohman <56929777+mgrohman13@users.noreply.github.com>
2023-02-22 18:26:53 -0800

    Warpips Replayability Mod

---
+++ WarpipsReplayability/DefaultResourceSetter_SetNewConquestModeResources.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using BepInEx;
+using HarmonyLib;
+using GameUI;
+using System.Runtime.CompilerServices;
+
+namespace WarpipsReplayability
+{
+    [HarmonyPatch(typeof(DefaultResourceSetter))]
+    [HarmonyPatch("SetNewConquestModeResources")] // if possible use nameof() here
+    class DefaultResourceSetter_SetNewConquestModeResources
+    { 
+        //static bool Prefix(DefaultResourceSetter __instance, ref ResourceController ___resourceController)
+        //{
+        //    Plugin.Log.LogInfo("Prefix"); 
+        //    return true;
+        //}
+
+        static void Postfix(ref ResourceController ___resourceController)
+        {
+            ___resourceController.HardSetPlayerLives(2, 3);
+            Plugin.Log.LogInfo("HardSetPlayerLives(2, 3)"); 
+        }
+    } 
+}
---
+++ WarpipsReplayability/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Transactions;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace WarpipsReplayability
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Warpips.exe")]
+
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Log = base.Logger;
+
+            Harmony harmony = new Harmony("WarpipsReplayability.mod");
+            harmony.PatchAll();
+
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        public static ManualLogSource Log { get; private set; }
+    }
+}
