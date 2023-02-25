8cf3c8f63fb925c853c56aeca1fd52cbac5ead75
Matthew Grohman <56929777+mgrohman13@users.noreply.github.com>
2023-02-25 09:36:19 -0800

    gain extra tech on life reward when at max life

---
+++ WarpipsReplayability/ExtraLifeItemRewardController_TryPushExtraLife.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using HarmonyLib;
+using GameUI;
+using System.Runtime.CompilerServices;
+using LevelGeneration.WorldMap;
+using LevelGeneration;
+
+namespace WarpipsReplayability
+{
+    [HarmonyPatch(typeof(ExtraLifeItemRewardController))]
+    [HarmonyPatch(nameof(ExtraLifeItemRewardController.TryPushExtraLife))]
+    internal class ExtraLifeItemRewardController_TryPushExtraLife
+    {
+        private static int lives = -1;
+
+        public static void Prefix(ref ResourceController ___resourceController)
+        {
+            try
+            {
+                Plugin.Log.LogInfo("ExtraLifeItemRewardController_TryPushExtraLife Prefix");
+
+                lives = ___resourceController.PlayerLives.value;
+            }
+            catch (Exception e)
+            {
+                Plugin.Log.LogError(e);
+            }
+        }
+        public static void Postfix(ref ResourceController ___resourceController)
+        {
+            try
+            {
+                Plugin.Log.LogInfo("ExtraLifeItemRewardController_TryPushExtraLife Postfix");
+
+                if (lives == ___resourceController.PlayerLives.value)
+                {
+                    int points = Plugin.Rand.RangeInt(6, 9);
+                    ___resourceController.AddTechPoints(points);
+                    lives = -1;
+
+                    Plugin.Log.LogInfo($"Bonus max life tech points {points}");
+                }
+            }
+            catch (Exception e)
+            {
+                Plugin.Log.LogError(e);
+            }
+        }
+    }
+}
