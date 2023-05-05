d89f2a66ad968c21341ee5174af9cb21015ccef1
Matthew Grohman <56929777+mgrohman13@users.noreply.github.com>
2023-05-04 19:47:58 -0700

    set Map.MissionManagerAsset earlier on new game

---
+++ WarpipsReplayability/Patches/GameInitializer_InitializeNewConquest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using HarmonyLib;
+using GameUI;
+using System.Runtime.CompilerServices;
+using LevelGeneration.WorldMap;
+using WarpipsReplayability.Mod;
+using LevelGeneration;
+
+namespace WarpipsReplayability.Patches
+{
+    [HarmonyPatch(typeof(GameInitializer))]
+    [HarmonyPatch(nameof(GameInitializer.InitializeNewConquest))]
+    internal class GameInitializer_InitializeNewConquest
+    {
+        public static void Prefix(MissionManagerAsset ___missionManagerAsset)
+        {
+            try
+            {
+                Plugin.Log.LogDebug("GameInitializer_InitializeNewConquest Postfix");
+                Map.MissionManagerAsset = ___missionManagerAsset;
+            }
+            catch (Exception e)
+            {
+                Plugin.Log.LogError(e);
+            }
+        }
+    }
+}
