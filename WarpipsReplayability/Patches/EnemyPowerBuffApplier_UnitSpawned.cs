d0a13fdd02f05579bc07ad2d6cd7ec0b1923fafc
Matthew Grohman <56929777+mgrohman13@users.noreply.github.com>
2023-05-10 18:00:54 -0700

    don't skip SuperEnemyBuff on Sergeant difficulty

---
+++ WarpipsReplayability/Patches/EnemyPowerBuffApplier_UnitSpawned.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WarpipsReplayability.Mod;
+
+namespace WarpipsReplayability.Patches
+{
+    [HarmonyPatch(typeof(EnemyPowerBuffApplier))]
+    [HarmonyPatch("UnitSpawned")]
+    internal class EnemyPowerBuffApplier_UnitSpawned
+    {
+        public static void Prefix(EnemyPowerBuffApplier __instance, List<int> ___excludeOnDifficultyIndex)
+        {
+            try
+            {
+                Plugin.Log.LogDebug("EnemyPowerBuffApplier_UnitSpawned Prefix " + __instance.name);
+
+                if (__instance.name == "SuperEnemyBuff" && ___excludeOnDifficultyIndex.Count > 0)
+                {
+                    Plugin.Log.LogInfo(___excludeOnDifficultyIndex.Select(d => d.ToString()).Aggregate("", (a, b) => a + " " + b));
+                    ___excludeOnDifficultyIndex.Clear();
+                }
+            }
+            catch (Exception e)
+            {
+                Plugin.Log.LogError(e);
+            }
+        }
+    }
+}
