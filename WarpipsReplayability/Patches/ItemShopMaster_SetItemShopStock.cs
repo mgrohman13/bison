a8deedc6b313e59c5e71df559c4fe416f123716e
Matthew Grohman <56929777+mgrohman13@users.noreply.github.com>
2023-06-02 12:32:14 -0700

    load ItemShopMaster.SaleIndex

---
+++ WarpipsReplayability/Patches/ItemShopMaster_SetItemShopStock.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using WarpipsReplayability.Mod;
+
+namespace WarpipsReplayability.Patches
+{
+    [HarmonyPatch(typeof(ItemShopMaster))]
+    [HarmonyPatch(nameof(ItemShopMaster.SetItemShopStock))]
+    internal class ItemShopMaster_SetItemShopStock
+    {
+        private static readonly MethodInfo _saleIndex = AccessTools.PropertySetter(typeof(ItemShopMaster), "SaleIndex");
+
+        public static void Postfix(ItemShopMaster __instance)
+        {
+            try
+            {
+                Plugin.Log.LogDebug("ItemShopMaster_SetItemShopStock Postfix");
+
+                int saleIndex = Persist.Instance.SaleIndex;
+                Plugin.Log.LogInfo($"loaded SaleIndex {__instance.SaleIndex} -> {saleIndex}");
+                _saleIndex.Invoke(__instance, new object[] { saleIndex });
+            }
+            catch (Exception e)
+            {
+                Plugin.Log.LogError(e);
+            }
+        }
+    }
+}
