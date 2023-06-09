43e64a1cdcdffb61f4a14027ca20fd9dcaf0d8cd
Matthew Grohman <56929777+mgrohman13@users.noreply.github.com>
2023-06-09 15:13:55 -0700

    refactor ResourceController_AddXpPoints

--- WarpipsReplayability/Patches/ResourceController_AddXpPoints.cs
+++ WarpipsReplayability/Patches/ResourceController_AddXpPoints.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using TechTree;
 using WarpipsReplayability.Mod;
+using static TechTreeMaster;
 
 namespace WarpipsReplayability.Patches
 {
@@ -42,42 +43,14 @@ namespace WarpipsReplayability.Patches
 
                 if (runPrefix)
                 {
-                    int upgrades = 0;
-                    if (___playerTechTree != null)
-                        upgrades = ___xpAddons.Where(___playerTechTree.ReturnIsTechUnlocked).Count();
-                    upgrades *= xpModifierPerUpgrade;
-                    int tier = ___techTier.value * xpModifierPerTier;
-
-                    ////game algorithm
-                    //addValue = Plugin.Rand.Round((addValue * ___xpModifierOnKill.Value) + upgrades + tier);
-
-                    //rebalance experience by giving less for basic units and more for better ones
-                    float value = (float)Math.Pow(addValue * xpMult, xpExponent);
-                    //apply bonuses as percentages, not additions 
-                    value *= xpModifierOnKill + upgrades + tier;
-
-                    float dev = (float)(.39 / Math.Sqrt(value));
-                    int xp = Plugin.Rand.GaussianCappedInt(value, dev, 1);
-                    Log(xp, value, dev, upgrades, tier);
-
-                    //there is a bug in AddXpPoints if you try to add more experience than needed for a single level
-                    //you will be under-charged for subsequent levels after the first
-                    //to fix this we call it manually in chunks but return the total amount added from this first call
-                    __result = xp;
+                    //let recursive ResourceController.AddXpPoints calls run the base game code
                     runPrefix = false;
-                    while (xp > 0)
-                    {
-                        int level = __instance.ReturnXPNeededToLevel();
-                        int add = Math.Min(xp, level);
-                        Plugin.Log.LogDebug($"AddXpPoints {add} ({level})");
-                        __instance.AddXpPoints(add);
-                        xp -= add;
 
-                        if (xp > 0)
-                            Plugin.Log.LogWarning($"AddXpPoints loop ({xp}, {level})");
-                    }
+                    int upgrades = GetUpgrades(___playerTechTree, ___xpAddons);
+                    int tier = GetTier(___techTier);
+                    __result = AddXpPoints(__instance, upgrades, tier, addValue);
+
                     runPrefix = true;
-
                     return false;
                 }
                 else
@@ -92,6 +65,39 @@ namespace WarpipsReplayability.Patches
             return true;
         }
 
+        private static int AddXpPoints(ResourceController instance, int upgrades, int tier, float addValue)
+        {
+            ////for comparison, game algorithm:
+            //int xp = (int)Math.Round((addValue * xpModifierOnKill) + upgrades + tier);
+
+            //rebalance experience by giving less for basic units and more for better ones
+            float value = (float)Math.Pow(addValue * xpMult, xpExponent);
+            //apply bonuses as percentages, not additions 
+            value *= xpModifierOnKill + upgrades + tier;
+
+            float dev = (float)(.39 / Math.Sqrt(value));
+            int xp = Plugin.Rand.GaussianCappedInt(value, dev, 1);
+            Log(xp, value, dev, upgrades, tier);
+
+            //there is a bug in AddXpPoints if you try to add more experience than needed for a single level
+            //you will be under-charged for subsequent levels after the first
+            //to fix this we call it manually in chunks but return, from this first call, the total amount added 
+            int result = xp;
+            while (xp > 0)
+            {
+                int level = instance.ReturnXPNeededToLevel();
+                int add = Math.Min(xp, level);
+                Plugin.Log.LogDebug($"AddXpPoints {add} ({level})");
+                instance.AddXpPoints(add);
+                xp -= add;
+
+                if (xp > 0)
+                    Plugin.Log.LogWarning($"AddXpPoints loop ({xp}, {level})");
+            }
+
+            return result;
+        }
+
         private static void SetValues(ref IntReadonlyStat ___xpModifierPerUpgrade, ref IntReadonlyStat ___xpModifierPerTier, ref FloatReadonlyStat ___xpModifierOnKill)
         {
             xpModifierPerUpgrade = ___xpModifierPerUpgrade.Value;
@@ -103,7 +109,18 @@ namespace WarpipsReplayability.Patches
             _gameStat_int.SetValue(___xpModifierPerTier, 0);
             _gameStat_float.SetValue(___xpModifierOnKill, 1);
         }
-
+        private static int GetUpgrades(TechTreeInstance playerTechTree, List<TechType> xpAddons)
+        {
+            int upgrades = 0;
+            if (playerTechTree != null)
+                upgrades = xpAddons.Where(playerTechTree.ReturnIsTechUnlocked).Count();
+            upgrades *= xpModifierPerUpgrade;
+            return upgrades;
+        }
+        private static int GetTier(IntResource techTier)
+        {
+            return techTier.value * xpModifierPerTier;
+        }
         private static void Log(int xp, float value, float dev, int upgrades, int tier)
         {
             string info = $"({value:0.00},{value * dev:0.00}), upgrades {upgrades}, tier {tier})";
